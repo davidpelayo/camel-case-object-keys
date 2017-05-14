c7e354e625a1b521dafefc38ac4414e1dcf4b822
David Pelayo <hi@davidpelayo.com>
2017-05-14 11:50:08 +0200

    Simplifying

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-import isObject from 'lodash/isObject';
 import mapKeys from 'lodash/mapKeys';
 import camelCase from 'lodash/camelCase';
 
-function camelCaseObjectKeys(object) {
-  const returnObject = {};
-
-  if (!isObject(object)) {
-    return returnObject;
-  }
-
-  return mapKeys(object, (v, k) => { return camelCase(k); });
-}
+const camelCaseObjectKeys = (o) => mapKeys(o, (v, k) => camelCase(k));
 
 export default camelCaseObjectKeys;
